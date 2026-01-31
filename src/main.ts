e10dbfbeedbeac65204aee5a072a505e4c566b99
iotutvecklare <iotutvecklare@gmail.com>
2026-01-31 05:24:41 +0100

    Renderer

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,56 @@
 import './style.css'
 import * as THREE from 'three'
-//import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+
+const scene = new THREE.Scene()
+
+//const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000)
+camera.position.z = 1.5
+
+/*
+The most common Renderer used in Three.js is the WebGLRenderer.
+It paints the scene and camera information onto a HTML Canvas Element.
+The WebGLRenderer will use WebGL.
+WebGL allows GPU-accelerated image processing and effects as the renderer creates the 2D image for the Canvas.
+ */
+
+const canvas = document.getElementById('canvas') as HTMLCanvasElement
+const renderer = new THREE.WebGLRenderer({ canvas: canvas })
+renderer.setSize(200, 200)
+
+
+// const renderer = new THREE.WebGLRenderer()
+// renderer.setSize(window.innerWidth, window.innerHeight)
+// document.body.appendChild(renderer.domElement)
+
+
+// window.addEventListener('resize', () => {
+//   camera.aspect = window.innerWidth / window.innerHeight
+//   camera.updateProjectionMatrix()
+//   renderer.setSize(window.innerWidth, window.innerHeight)
+// })
+
+new OrbitControls(camera, renderer.domElement)
+
+const geometry = new THREE.BoxGeometry()
+const material = new THREE.MeshNormalMaterial({ wireframe: true })
+
+const cube = new THREE.Mesh(geometry, material)
+scene.add(cube)
+
+function animate() {
+  requestAnimationFrame(animate)
+
+  renderer.render(scene, camera)
+}
+
+animate()
+
+/*
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import Stats from 'three/addons/libs/stats.module.js'
 import { GUI } from 'dat.gui'
 
@@ -16,10 +66,8 @@ const backgroundC = new THREE.CubeTextureLoader()
 
 scene.background = backgroundA
 
-const camera = new THREE.OrthographicCamera(-4, 4, 4, -4, -5, 10)
-camera.position.set(1, 1, 1)
-camera.lookAt(0, 0.5, 0)
-
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+camera.position.z = 1.5
 
 
 const renderer = new THREE.WebGLRenderer()
@@ -27,12 +75,12 @@ renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
 window.addEventListener('resize', () => {
-  //camera.aspect = window.innerWidth / window.innerHeight
+  camera.aspect = window.innerWidth / window.innerHeight
   camera.updateProjectionMatrix()
   renderer.setSize(window.innerWidth, window.innerHeight)
 })
 
-//new OrbitControls(camera, renderer.domElement)
+new OrbitControls(camera, renderer.domElement)
 
 const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshNormalMaterial({ wireframe: true })
@@ -63,27 +111,21 @@ cubeFolder.add(cube.rotation, 'y', 0, Math.PI * 2, 0.01)
 cubeFolder.add(cube.rotation, 'z', 0, Math.PI * 2, 0.01)
 cubeFolder.open()
 
-// Camera controls
+// Camera controls (works always because cube is always the same object)
 const cameraFolder = gui.addFolder('Camera')
-// cameraFolder.add(camera.position, 'x', -10, 10)
-// cameraFolder.add(camera.position, 'y', -10, 10)
-// cameraFolder.add(camera.position, 'z', -10, 10)
-cameraFolder.add(camera, 'left', -10, 0).onChange(() => {
+cameraFolder.add(camera.position, 'x', -10, 10)
+cameraFolder.add(camera.position, 'y', -10, 10)
+cameraFolder.add(camera.position, 'z', -10, 10)
+cameraFolder.add(camera, 'fov', 0, 180, 0.01).onChange(() => {
   camera.updateProjectionMatrix()
 })
-cameraFolder.add(camera, 'right', 0, 10).onChange(() => {
+cameraFolder.add(camera, 'aspect', 0.00001, 10).onChange(() => {
   camera.updateProjectionMatrix()
 })
-cameraFolder.add(camera, 'top', 0, 10).onChange(() => {
+cameraFolder.add(camera, 'near', 0.01, 10).onChange(() => {
   camera.updateProjectionMatrix()
 })
-cameraFolder.add(camera, 'bottom', -10, 0).onChange(() => {
-  camera.updateProjectionMatrix()
-})
-cameraFolder.add(camera, 'near', -5, 5).onChange(() => {
-  camera.updateProjectionMatrix()
-})
-cameraFolder.add(camera, 'far', 0, 10).onChange(() => {
+cameraFolder.add(camera, 'far', 0.01,10).onChange(() => {
   camera.updateProjectionMatrix()
 })
 cameraFolder.open()
@@ -98,3 +140,4 @@ function animate() {
 }
 
 animate()
+*/
